c1f57cbf7d1e97e476a35fa592cb4d299800b434
Simon de Haan <simon@praekeltfoundation.org>
2013-07-18 20:01:49 +0200

    clone & pull working

--- fabfile.py
+++ fabfile.py
@@ -1,36 +1,61 @@
-from fabric.api import cd, sudo, env
 import os
+import sys
+import yaml
+import urlparse
 
-expected_vars = [
-    'PROJECT',
-]
+from fabric.api import cd, sudo, env
+from fabric.context_managers import shell_env, prefix
+from fabric.contrib.files import exists
 
-for var in expected_vars:
-    if var not in os.environ:
-        raise Exception('Please specify %s environment variable' % (
-            var,))
+DEPLOY_FILE = os.environ.get('DEPLOY_FILE', '.deploy.yml')
+DEPLOY_USER = os.environ.get('DEPLOY_USER', 'jmbo')
 
-PROJECT = os.environ['PROJECT']
-USER = os.environ.get('USER', 'jmbo')
+recipe = yaml.load(open(DEPLOY_FILE, 'r'))
 
-env.path = os.path.join('var', 'praekelt', PROJECT)
+# vary shallow validations
+expected_keys = ['repository', 'hosts']
+
+if not set(expected_keys).issubset(set(recipe.keys())):
+    raise Exception('Invalid config, check the %s' % (DEPLOY_FILE,))
+    sys.exit(1)
+
+# which hosts to connect to
+env.hosts = recipe['hosts']
+
+# which repository to clone
+repository_url = urlparse.urlparse(recipe['repository'])
+repository_name = repository_url.path.split('/')[-1]
+folder_name = repository_name.rsplit('.', 1)[0]
+
+# path to where apps are hosted
+env.path = os.path.join('/', 'var', 'praekelt', folder_name)
+
+# which environment variables to set when stuff is run
+environment = recipe.get('env', {})
+
+# which processes to have supervisord start
+processes = recipe.get('processes', {})
+
+# actions
+post_clone = recipe.get('post_clone', [])
+post_pull = recipe.get('post_pull', [])
 
 
-def restart():
-    sudo('/etc/init.d/nginx restart')
-    sudo('supervisorctl reload')
-
-
-def deploy():
+def clone():
+    if not exists(env.path):
+        sudo('git clone %s %s' % (repository_url.geturl(), env.path),
+             user=DEPLOY_USER)
     with cd(env.path):
-        sudo('git pull', user=USER)
-        sudo('ve/bin/python manage.py syncdb --migrate --noinput',
-             user=USER)
-        sudo('ve/bin/python manage.py collectstatic --noinput',
-             user=USER)
+        sudo('virtualenv ve', user=DEPLOY_USER)
+        with prefix('. ve/bin/activate'):
+            for command in post_clone:
+                with shell_env(**environment):
+                    sudo(command, user=DEPLOY_USER)
 
 
-def install_packages(force=False):
+def pull():
     with cd(env.path):
-        sudo('ve/bin/pip install %s -r requirements.pip' % (
-             '--upgrade' if force else '',), user=USER)
+        with prefix('. ve/bin/activate'):
+            for command in post_pull:
+                with shell_env(**environment):
+                    sudo(command, user=DEPLOY_USER)
